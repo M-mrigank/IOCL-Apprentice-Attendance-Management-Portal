c5ea4db7a095b6373e6c44d794d46839633491d4
M-mrigank <mrigank.mgr@gmail.com>
2024-06-30 17:30:49 +0530

    changes

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -4,6 +4,13 @@ const mailSender=async(email, title, body)=>{
     try{
         let transporter=nodemailer.createTransport({
             host:process.env.MAIL_HOST,
+
+            port:465,
+            secure: true, 
+            logger: true,
+            debug: true,
+            secureConnection: false,
+
             auth:{
                 user:process.env.MAIL_USER,
                 pass:process.env.MAIL_PASS,
